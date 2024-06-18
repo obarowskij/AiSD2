8ea81102d06ed5e916a4b19a77a4b14b4c3fd8ab
Huntoper <41130596+Huntoper@users.noreply.github.com>
2024-06-18 11:56:04 +0200

    Update algorithm_guards.py

--- flatworld/functions/algorithm_guards.py
+++ flatworld/functions/algorithm_guards.py
@@ -1,10 +1,9 @@
 from random import randint
-from .models import Guard, Point
+from .models import Guard, Point, SegmentTree
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
 import io
-
 
 def get_images(hull_points, hull_points_energy, guard_data):
     hull_points = [Point(x, y) for x, y in hull_points]
@@ -43,44 +42,47 @@ def get_images(hull_points, hull_points_energy, guard_data):
 
     return images_data
 
-
-def find_stop_points(max_steps, hull_points_energy):
-    steps = 0
-    songs = []
+def find_stop_points(max_steps, hull_points_energy, segment_tree):
+    n = len(hull_points_energy)
     stop_points = []
-    for i in range(len(hull_points_energy)):
-        if steps == max_steps or (
-            i > 0 and hull_points_energy[i] < hull_points_energy[i - 1]
-        ):
-            stop_points.append(i + 1)
-            if i > 0 and hull_points_energy[i] > hull_points_energy[i - 1]:
-                songs.append(i + 1)
-            steps = 0
-        steps += 1
+    songs = []
+    i = 0
+
+    while i < n - 1:
+        min_value = segment_tree.range_min_query(i + 1, min(i + max_steps, n))
+        found = False
+        for j in range(i + 1, min(i + max_steps, n)):
+            if hull_points_energy[j] == min_value and hull_points_energy[j] < hull_points_energy[j-1]:
+                stop_points.append(j+1)
+                if j > 0 and hull_points_energy[j] > hull_points_energy[j-1]:
+                    songs.append(j+1)
+                i = j
+                found = True
+                break
+
+        if not found:
+            i = min(i + max_steps, n-1)
+            stop_points.append(i+1)
+            if hull_points_energy[i] > hull_points_energy[i-1]:
+                songs.append(i+1)
+
     if hull_points_energy[0] > hull_points_energy[-1]:
         songs.append(1)
     stop_points.append(1)
     return stop_points, songs
 
-
-def generate_flat_schedule(people, hull_points_number, max_steps, hull_points):
-    guards = [
-        Guard(i, randint(1, 100), max_steps) for i in range(1, people + 1)
-    ]
+def generate_flat_schedule(people, hull_points_number, max_steps,hull_points):
+    guards = [Guard(i, randint(1, 100), max_steps) for i in range(1, people + 1)]
     sorted_guards = sorted(guards, key=lambda x: x.energy, reverse=True)
-    # jak nie bedzie tych 7 straznikow to nie bedzie mial kto tam patrolowac brak danych jakis case dorobic
-    chosen_guards = [guard for guard in sorted_guards][:7] # if guard.energy > 50
+    chosen_guards = [guard for guard in sorted_guards][:7]
     guard_data = []
     for day, guard in enumerate(chosen_guards, start=1):
         guard.set_patrol_day(day)
         max_steps = guard.max_steps
-        hull_points_energy = [
-            randint(1, 100) for _ in range(hull_points_number)
-        ]
-        stop_points, songs = find_stop_points(max_steps, hull_points_energy)
-        print(
-            f"Strażnik {guard.id} zatrzymuje się na punktach {stop_points}, melodie: {songs} w dniu {day}"
-        )
+        hull_points_energy = [randint(1, 100) for _ in range(hull_points_number)]
+        segment_tree = SegmentTree(hull_points_energy)
+        stop_points, songs = find_stop_points(max_steps, hull_points_energy,segment_tree)
+        print(f"Strażnik {guard.id} zatrzymuje się na punktach {stop_points}, melodie: {songs} w dniu {day}")
         guard_data.append(
             {
                 "id": guard.id,
@@ -92,4 +94,4 @@ def generate_flat_schedule(people, hull_points_number, max_steps, hull_points):
         )
     images_data = get_images(hull_points, hull_points_energy, guard_data)
     # id strraznika liste punktow i cos tam
-    return guard_data, images_data
+    return guard_data,images_data
